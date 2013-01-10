5cee139e3e3b786389c6e6c8659031198acd64da
Lex Podgorny <ap.public1@gmail.comap.public1@gmail.com>
2013-01-10 15:55:29 -0800

    Removing files

--- decorators/package.js
+++ decorators/package.js
@@ -3,7 +3,6 @@ enyo.depends(
 	'kind.Spotlight.Decorator.List.js',
 	'kind.Spotlight.Decorator.Checkbox.js',
 	'kind.Spotlight.Decorator.Slider.js',
-	'kind.Spotlight.Decorator.RangeSlider.js',
 	'kind.Spotlight.Decorator.Picker.js',
 	'kind.Spotlight.Decorator.Input.js'
 );
