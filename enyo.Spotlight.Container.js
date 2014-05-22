31700d9ab2f34c534c406a8f6e2d5db601fe6f7c
Cho Sungbae <sb.cho@lge.com>
2014-05-22 14:24:31 -0700

    modify _handleEvent function to enable that spotlight container can be cached

--- enyo.Spotlight.Container.js
+++ enyo.Spotlight.Container.js
@@ -39,6 +39,7 @@ enyo.Spotlight.Container = new function() {
 
 		// Handle events bubbling from within the container
 		_handleEvent = function(oSender, oEvent) {
+			oSender.cachePoint = true;
 			switch (oEvent.type) {
 				case 'onSpotlightFocus':
 					if (oEvent.originator !== oSender) {
